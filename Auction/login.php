aaf80d38dcca09d5f460681dfad85329e5124145
Kyle <kokleRun@gmail.com>
2016-09-27 14:04:22 -0500

    Setting Up Picture Upload
    I have the code ready. I think I just now need to change permissions
    for /var/www/projects/AM_Auction/pictures so “apache” owns it…

--- Auction/login.php
+++ Auction/login.php
@@ -40,5 +40,33 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
+echo '<br>';
+$target_dir = "pictures/";
+$target_file = $target_dir . basename($_FILES["picture"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["picture"]["name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["picture"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
 $conn->close();
 ?>
